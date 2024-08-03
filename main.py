c73d5c1f7789e72117c381a3ddb92a1079482558
Amanda Li <a383li@uwaterloo.ca>
2024-08-02 22:33:12 -0400

    removing normalization

--- main.py
+++ main.py
@@ -29,14 +29,14 @@ data_transforms = {
         transforms.RandomResizedCrop(input_size),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        
     ]),
     'val': transforms.Compose([
         transforms.Lambda(preprocess_image),  # Apply preprocessing
         transforms.Resize(input_size),
         transforms.CenterCrop(input_size),
         transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+       
     ]),
 }
 
